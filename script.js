6525e61668d06b85607092264557f1e2e5be176c
Lucas <lucas.le-bodo@epitech.eu>
2021-05-14 14:36:18 +0200

    create project

---
+++ script.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const prompt = require('prompt-sync')();
+
+console.log('Bienvenue dans ' + chalk.blue('IMC Calculateur') + '.');
+console.log('Vous pouvez calculer votre ' + chalk.green('IMC') + ' en toute simplicité.\n');
+
+
+
