e93f6d7ccd3c90fca7d1ec0b429e82ca6ef7a1c1
Lucas <lucas.le-bodo@epitech.eu>
2021-05-14 17:10:32 +0200

    create input, and add calc imc with response

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const chalk = require('chalk');
-const prompt = require('prompt-sync')();
+const prompts = require('prompts');
+const result = require('./result.json');
 
-console.log('Bienvenue dans ' + chalk.blue('IMC Calculateur') + '.');
-console.log('Vous pouvez calculer votre ' + chalk.green('IMC') + ' en toute simplicité.\n');
+const questions = [{
+        type: 'number',
+        name: 'taille',
+        message: 'Quelle est votre taille ? (cm)'
+    },
+    {
+        type: 'number',
+        name: 'poids',
+        message: 'Quel est votre taille ? (Kg)'
+    }
+];
 
+(async () => {
+    const reponse = await prompts(questions);
+    let taille = reponse['taille'] * 0.01;
+    let calcul = reponse['poids']/(taille * taille);
+    let imc = Math.round(calcul * 100) / 100;
 
+    console.log(`Votre IMC est de ${imc}`)
 
+    if(imc < 18.5 ){
+        console.log(result.reponse1);
+    } else if(18.5 < imc < 24.9){
+        console.log(result.reponse2);
+    } else if(25 < imc < 29.9){
+        console.log(result.reponse3);
+    } else if(30 < imc < 34.9){
+        console.log(result.reponse4);
+    } else if(35 < imc < 39.9){
+        console.log(result.reponse5);
+    } else if(imc > 40){
+        console.log(result.reponse6);
+    }
+})();
